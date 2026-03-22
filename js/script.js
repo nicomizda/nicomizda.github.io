02994963b0b8d18baa91dbc7246fd14d61674605
nicomizda <83329875+nicomizda@users.noreply.github.com>
2026-03-23 00:31:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 /*FOR HEADER*/
+/* HEADER読み込み */
+fetch("./component/header.html")
+  .then(response => response.text())
+  .then(data => {
+    const header = document.getElementById("header");
+    if (header !== null) {
+      header.innerHTML = data;
+    } else {
+      console.error("header要素が見つかりません");
+    }
+  });
+
+/* FOOTER読み込み */
+fetch("./component/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    const footer = document.getElementById("footer");
+    if (footer !== null) {
+      footer.innerHTML = data;
+    } else {
+      console.error("footer要素が見つかりません");
+    }
+  });
+
+/*　旧ヘッダーフッター読み込み
 fetch("../component/header.html")
   .then(response => response.text())
   .then(data => {
@@ -9,17 +34,22 @@ fetch("../component/footer.html")
   .then(response => response.text())
   .then(data => {
   document.getElementById("footer").innerHTML = data;
-});
+});*/
 
 function openModal(img){
   const modal = document.getElementById("modal");
   const modalImg = document.getElementById("modalImg");
-  modalImg.src = img.src;
-  modal.classList.add("active");
+  if (modal && modalImg) {
+    modalImg.src = img.src;
+    modal.classList.add("active");
+  } else {
+    console.error("modal要素が見つかりません");
+  }
 }
 
 function closeModal(){
-  /*document.getElementById("modal").style.display = "none";*/
   const modal = document.getElementById("modal");
-  modal.classList.remove("active");
+  if (modal) {
+    modal.classList.remove("active");
+  }
 }
