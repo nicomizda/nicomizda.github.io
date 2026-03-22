02ac2e53165b6a95f8547e6cd247916600337b20
nicomizda <83329875+nicomizda@users.noreply.github.com>
2026-03-22 23:38:35 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 /*FOR HEADER*/
 fetch("/component/header.html")
-.then(response => response.text())
-.then(data => {
-document.getElementById("header").innerHTML = data;
+  .then(response => response.text())
+  .then(data => {
+  document.getElementById("header").innerHTML = data;
+});
+
+fetch("/component/footer.html")
+  .then(response => response.text())
+  .then(data => {
+  document.getElementById("footer").innerHTML = data;
 });
 
 function openModal(img){
