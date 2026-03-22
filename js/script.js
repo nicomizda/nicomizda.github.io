1b3cb2b8daf76d4e165822ff18cc306163bbec46
nicomizda <83329875+nicomizda@users.noreply.github.com>
2026-03-23 00:24:47 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 /*FOR HEADER*/
-fetch("/component/header.html")
+fetch("../component/header.html")
   .then(response => response.text())
   .then(data => {
   document.getElementById("header").innerHTML = data;
 });
 
-fetch("/component/footer.html")
+fetch("../component/footer.html")
   .then(response => response.text())
   .then(data => {
   document.getElementById("footer").innerHTML = data;
